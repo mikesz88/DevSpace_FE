3982755f565454acedbe3bd8534dfd3bc4672b7e
Richard Olpindo <djricky088@yahoo.com>
2023-11-17 17:14:19 -1000

    Removed aref from HTML page

--- Devspace-Delete/deleteMe.js
+++ Devspace-Delete/deleteMe.js
@@ -1,6 +1,7 @@
 function deleteMe() {
 fetch("https://devspace.cyclic.app/api/v1/auth/deleteMe",{
   method: 'DELETE',
-}).then (response => response.json)
-.catch((error)=>{console.log(`ERROR: ${error}`)});
+}).then (response => response.json())
+.then(alert(response.message))
+.catch((error)=>{alert(`ERROR: ${error}`)});
 }
